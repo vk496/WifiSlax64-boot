c3e05576f4d300a70b43bf2162d1f7964447b200
Val <vk18496@gmail.com>
2016-08-24 12:46:01 +0200

    Despliegue UEFI

--- make_boot.sh
+++ make_boot.sh
@@ -10,8 +10,6 @@ fi
 ### ROOT ###
 
 
-
-
 ############## Obtener ruta absoluta del directorio donde nos encontramos ##############
 
 SOURCE="${BASH_SOURCE[0]}"
@@ -50,3 +48,28 @@ rm -Rf $DIR/ISO_BOOT/boot/initrd
 ### Initrd ###
 
 
+
+
+### EFI ###
+
+# Generamos la imagen EFI
+grub-mkimage --format=x86_64-efi --output=$DIR/ISO_BOOT/EFI/BOOT/bootx64.efi --config=$DIR/ISO_BOOT/EFI/BOOT/grub-embedded.cfg --compression=xz --prefix=/EFI/BOOT part_gpt part_msdos fat ext2 hfs hfsplus iso9660 udf ufs1 ufs2 zfs chain linux boot appleldr ahci configfile normal regexp minicmd reboot halt search search_fs_file search_fs_uuid search_label gfxterm gfxmenu efi_gop efi_uga all_video loadbios gzio echo true probe loadenv bitmap_scale font cat help ls png jpeg tga test at_keyboard usb_keyboard
+
+if [ $? -ne 0 ]; then
+	echo "La imagen EFI no se generó correctamente"
+	exit 1
+fi
+
+#Creamos el efiboot.img
+dd if=/dev/zero of=$DIR/ISO_BOOT/EFI/BOOT/efiboot.img bs=1K count=1440
+mkdosfs -F 12 $DIR/ISO_BOOT/EFI/BOOT/efiboot.img
+MOUNTPOINT=$(mktemp -d)
+mount -o loop $DIR/ISO_BOOT/EFI/BOOT/efiboot.img $MOUNTPOINT
+mkdir -p $MOUNTPOINT/EFI/BOOT
+cp -a $DIR/ISO_BOOT/EFI/BOOT/bootx64.efi $MOUNTPOINT/EFI/BOOT
+umount $MOUNTPOINT
+rmdir $MOUNTPOINT
+mv $DIR/ISO_BOOT/EFI/BOOT/efiboot.img $DIR/ISO_BOOT/boot/syslinux/
+
+
+### EFI ###
