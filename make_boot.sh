f8d480f802faaf49dc96669b89ea536a0374c2f3
Val <vk18496@gmail.com>
2016-08-28 13:32:21 +0200

    Linux installer + fix target dir to Fat32

--- make_boot.sh
+++ make_boot.sh
@@ -11,18 +11,15 @@ fi
 
 ### DEPENDENCIAS ###
 
+DEPENDENCIAS=( grub-mkimage mkdosfs makeself )
 EXIT=1
 
-if ! hash grub-mkimage 2>/dev/null; then
-	echo "grub-mkimage not installed"
-	EXIT=0
-fi
-
-if ! hash mkdosfs 2>/dev/null; then
-	echo "mkdosfs not installed"
-	EXIT=0
-fi
-
+for var in "${DEPENDENCIAS[@]}"; do
+	if ! hash $var 2>/dev/null; then
+		echo "$var not installed"
+		EXIT=0
+	fi
+done
 
 if [ $EXIT -eq 0 ]; then
 	exit 1
@@ -57,7 +54,8 @@ if [ $# -gt 1 ]; then
 else
 
 	if [ $# -eq 1 ] && [[ -d $1 ]]; then
-		OUTPUT=$1
+		OUTPUT=$(mktemp -d)
+		FINAL_OUTPUT=$1
 	else
 		echo -e "You can set a target folder with the first argument\n"
 		OUTPUT=$DIR/ISO_BOOT
@@ -74,16 +72,16 @@ if [ $OUTPUT = $DIR/ISO_BOOT ]; then
 	rm -Rf $OUTPUT
 	mkdir -p $OUTPUT
 else
-	if [[ -d $OUTPUT/boot ]]; then
+	if [[ -d $FINAL_OUTPUT/boot ]]; then
 		num=$RANDOM
-		echo "boot alredy exists in $OUTPUT. Backup folder to boot_$num"
-		mv $OUTPUT/boot $OUTPUT/boot_$num
+		echo "boot alredy exists in $FINAL_OUTPUT. Backup folder to boot_$num"
+		mv $FINAL_OUTPUT/boot $FINAL_OUTPUT/boot_$num
 	fi
 
-	if [[ -d $OUTPUT/EFI ]]; then
+	if [[ -d $FINAL_OUTPUT/EFI ]]; then
 		num=$RANDOM
-		echo "EFI alredy exists in $OUTPUT. Backup folder to EFI_$num"
-		mv $OUTPUT/EFI $OUTPUT/EFI_$num
+		echo "EFI alredy exists in $FINAL_OUTPUT. Backup folder to EFI_$num"
+		mv $FINAL_OUTPUT/EFI $FINAL_OUTPUT/EFI_$num
 	fi
 
 	if [ $num ]; then
@@ -102,9 +100,9 @@ echo -en "Initrd .."
 find $OUTPUT/boot/initrd | cpio -o -H newc 2>/dev/null | xz -z -9 --check=crc32 > $OUTPUT/boot/initrd.xz >/dev/null
 
 if [[ ${PIPESTATUS[2]} -eq 0 ]] && [[ ${PIPESTATUS[2]} -eq 0 ]]; then
-	echo -e "\tok"
+	echo -e "\t\tok"
 else
-	echo -e "\terror"
+	echo -e "\t\terror"
 	exit 1
 fi
 
@@ -121,9 +119,9 @@ echo -en "UEFI loader .."
 grub-mkimage --format=x86_64-efi --output=$OUTPUT/EFI/BOOT/bootx64.efi --config=$OUTPUT/EFI/BOOT/grub-embedded.cfg --compression=xz --prefix=/EFI/BOOT part_gpt part_msdos fat ext2 hfs hfsplus iso9660 udf ufs1 ufs2 zfs chain linux boot appleldr ahci configfile normal regexp minicmd reboot halt search search_fs_file search_fs_uuid search_label gfxterm gfxmenu efi_gop efi_uga all_video loadbios gzio echo true probe loadenv bitmap_scale font cat help ls png jpeg tga test at_keyboard usb_keyboard >/dev/null
 
 if [ $? -eq 0 ]; then
-	echo -e "\tok"
+	echo -e "\t\tok"
 else
-	echo -e "\terror"
+	echo -e "\t\terror"
 	exit 1
 fi
 
@@ -142,12 +140,47 @@ rmdir $MOUNTPOINT; let EXIT=$EXIT+$?
 mv $OUTPUT/EFI/BOOT/efiboot.img $OUTPUT/boot/syslinux/; let EXIT=$EXIT+$?
 
 if [ $EXIT -eq 0 ]; then
+	echo -e "\t\tok"
+else
+	echo -e "\t\terror"
+	exit 1
+fi
+
+### EFI ###
+
+
+### Linux installer ###
+
+mkdir -p $OUTPUT/boot/tmp
+mv $OUTPUT/boot/wifislax_bootloader_installer $OUTPUT/boot/tmp/.wifislax_bootloader_installer
+
+echo -en "Linux installer .."
+EXIT=0
+
+makeself --target . --notemp $OUTPUT/boot/tmp/ $OUTPUT/boot/Linux_Wifislax_Boot_Installer.com "Wifislax Bootloader Installer" ".wifislax_bootloader_installer/bootinst.com" &>/dev/null
+
+if [ $? -eq 0 ]; then
 	echo -e "\tok"
 else
 	echo -e "\terror"
 	exit 1
 fi
 
-echo -e "\nBOOT FOLDER: $OUTPUT"
+chmod 444 $OUTPUT/boot/Linux_Wifislax_Boot_Installer.com
 
-### EFI ###
+rm -Rf $OUTPUT/boot/tmp
+
+### Linux installer ###
+
+
+### Copy if path specified ###
+
+if [ $FINAL_OUTPUT ]; then
+	cp -Rf $OUTPUT/* $FINAL_OUTPUT/
+	rm -Rf $OUTPUT
+fi
+
+### Copy if path specified ###
+
+
+echo -e "\nBOOT FOLDER: $FINAL_OUTPUT"
